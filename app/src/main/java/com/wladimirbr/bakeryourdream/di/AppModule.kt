485f5b3b3f06a16416fc7648293d2109a8482e59
Wladimir Bandeira <tecnico100@gmail.com>
2023-09-04 11:24:14 -0300

    configurando a injeção de dependências

---
+++ app/src/main/java/com/wladimirbr/bakeryourdream/di/AppModule.kt
@@ -0,0 +1,18 @@
+package com.wladimirbr.bakeryourdream.di
+
+
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+
+}
