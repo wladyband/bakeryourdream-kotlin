0ea8fa30f89cab176c9414c43de701b2bc5152d1
Wladimir Bandeira <tecnico100@gmail.com>
2023-09-06 11:06:56 -0300

    organizando navegação

--- app/src/main/java/com/wladimirbr/bakeryourdream/screens/login/LoginScreen.kt
+++ app/src/main/java/com/wladimirbr/bakeryourdream/screens/login/LoginScreen.kt
@@ -1,6 +1,7 @@
 package com.wladimirbr.bakeryourdream.screens.login
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
 
 @Composable
-fun LoginScreen(){}
+fun LoginScreen(navController: NavController){}
